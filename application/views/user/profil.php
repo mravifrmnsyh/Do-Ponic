ed71567d741115e80cb3289966f80203e60ff9bd
M Ilyas Arman S <milyasarmans13@gmail.com>
2021-06-14 11:32:38 +0700

    Create view_profil

---
+++ application/views/user/profil.php
@@ -0,0 +1,31 @@
+<div class="container rounded bg-white mt-5 mb-5">
+    <div class="row">
+        <div class="col-md-3 border-right">
+            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" src="<?=base_url()?>assets/gambar/user/avatar.png"><span class="font-weight-bold">Nama User</span><span class="text-black-50">Username</span><span> </span></div>
+        </div>
+        <div class="col-md-5 border-right">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h4 class="text-right">Pengaturan Profil</h4>
+                </div>
+                <div class="row mt-2">
+                    <div class="col-md-6"><label class="labels">Nama</label><input type="text" class="form-control" placeholder="nama" value=""></div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-12"><label class="labels">Username</label><input type="text" class="form-control" placeholder="username" value=""></div>
+                    <div class="col-md-12"><label class="labels">Alamat</label><input type="text" class="form-control" placeholder="alamat" value=""></div>
+                </div>
+                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Simpan Profil</button></div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center experience"><span>Ubah Password</span><span class="border px-3 p-1 add-experience"><i class="fa fa-key"></i>&nbsp;Password</span></div><br>
+                <div class="col-md-12"><label class="labels">Password</label><input type="text" class="form-control" placeholder="password" value=""></div> <br>
+                <div class="col-md-12"><label class="labels">Konfirmasi</label><input type="text" class="form-control" placeholder="konfirmasi" value=""></div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
