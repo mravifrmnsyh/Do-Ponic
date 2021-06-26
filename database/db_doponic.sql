e557df0c7ddda10d109c7dba4cdc00c7af1e78fe
ravifirmansyah10 <ravifirmansyah0@gmail.com>
2021-06-26 07:51:46 +0700

    databasenew

--- database/db_doponic.sql
+++ database/db_doponic.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2021 at 07:59 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+-- Waktu pembuatan: 26 Jun 2021 pada 02.45
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_artikel`
+-- Struktur dari tabel `tb_artikel`
 --
 
 CREATE TABLE `tb_artikel` (
@@ -37,7 +37,7 @@ CREATE TABLE `tb_artikel` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_artikel`
+-- Dumping data untuk tabel `tb_artikel`
 --
 
 INSERT INTO `tb_artikel` (`id_artikel`, `date`, `judul`, `tag`, `deskripsi`, `gambar`) VALUES
@@ -48,7 +48,7 @@ INSERT INTO `tb_artikel` (`id_artikel`, `date`, `judul`, `tag`, `deskripsi`, `ga
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_barang`
+-- Struktur dari tabel `tb_barang`
 --
 
 CREATE TABLE `tb_barang` (
@@ -62,7 +62,7 @@ CREATE TABLE `tb_barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_barang`
+-- Dumping data untuk tabel `tb_barang`
 --
 
 INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
@@ -74,37 +74,58 @@ INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`
 (6, 'Bawang Putih', 'Bawang Putih 1pcs/kg', 'buah', '40000', 20, 'product-11.jpg'),
 (7, 'Cabe Merah', 'Cabe Merah Segar 1pcs/kg', 'buah', '75000', 35, 'product-12.jpg'),
 (8, 'Kol Ungu', 'Kol Ungu Segar 1pcs/kg', 'buah', '35000', 50, 'product-4.jpg'),
-(9, 'Benih Cabe Merah', 'Benih berkualitas tinggi dengan tingkat kecambah 93%', 'benih', '25000', 7, 'product-121.jpg'),
 (10, 'Net Pot', 'Pot bersih adalah bahan pokok bagi setiap penanam hidroponik. Jaring yang lebih rapat di pot jaring Anda memungkinkan pilihan media tanam yang lebih luas, karena Anda dapat menggunakan media kecil.', 'alat', '7000', 156, 'alat-01.jpg'),
 (11, 'Net Pot Tray Insert 32 Lubang', 'Nampan jaring jaring kami dibuat dengan plastik polipropilena yang kuat untuk membantu memberi ketenangan pada pertanian Anda dengan umur panjang peralatan Anda. Dirancang agar sesuai dengan pot jaring 32, baki ini sangat cocok. Mereka juga berjarak sekitar ½ inci untuk memungkinkan ruang tumbuh yang cukup.', 'alat', '15000', 543, 'alat-02.jpg'),
 (12, 'Plastik Green House', 'Harga per Meter Perlindungan anti-debu - Mengurangi penyerapan debu untuk transmisi yang tahan lama Peringkat & Garansi 4 Tahun Transmisi cahaya > 90% Difusi Cahaya 20?itif Anti Kabut dan Anti Tetes Garansi 4 Tahun menjadikan NRCS plastik ini Lebih disukai Sebagian besar potongan dipotong sesuai spesifikasi.', 'alat', '15000', 1513, 'alat-03.jpg'),
-(13, 'Jaring 30%', 'Jaring pelindung rumah kaca kami tahan UV, tahan busuk dan jamur, dan memiliki garansi 8 tahun dengan harapan hidup 16 tahun. Jaring pelindung adalah komponen penting dari rumah kaca Anda untuk menjaga tanaman Anda aman selama musim panas yang intens. Harga adalah harga per-Meter.', 'alat', '15000', 1234, 'alat-04.jpg');
+(13, 'Jaring 30%', 'Jaring pelindung rumah kaca kami tahan UV, tahan busuk dan jamur, dan memiliki garansi 8 tahun dengan harapan hidup 16 tahun. Jaring pelindung adalah komponen penting dari rumah kaca Anda untuk menjaga tanaman Anda aman selama musim panas yang intens. Harga adalah harga per-Meter.', 'alat', '15000', 1234, 'alat-04.jpg'),
+(14, 'Benih Timun', 'Lama pertumbuhan sekitar 3 bulan untuk siap berbuah dan 2 bulan untuk berbuah', 'benih', '35000', 34, 'Benih_Timun-min.png'),
+(15, 'Benih Tomat', 'Paket benih dalam bentuk polybag', 'benih', '50000', 999, 'Benih_Tomat-min.png'),
+(16, 'Benih Wortel', 'Paket benih dalam bentuk polybag', 'benih', '50000', 999, 'Benih_Wortel-min.png'),
+(17, 'Benih Kentang', 'Paket benih dalam bentuk polybag', 'benih', '65000', 999, 'Benih_Kentang-min.png'),
+(18, 'Benih Lemon', 'Paket benih dalam bentuk polybag', 'benih', '75000', 999, 'Benih_Lemon-min.png'),
+(19, 'Benih Cabai', 'Paket benih dalam bentuk polybag', 'benih', '65000', 999, 'Benih_Cabai-min.png'),
+(20, 'Benih Mangga', 'Paket benih dalam bentuk polybag', 'benih', '95000', 999, 'Benih_Mangga-min.png'),
+(21, 'Benih Mint', 'Paket benih dalam bentuk polybag', 'benih', '65000', 999, 'Benih_Mint-min.png'),
+(22, 'Benih Stroberi', 'Paket benih dalam bentuk polybag', 'benih', '75000', 999, 'Benih_Stroberi-min.png'),
+(23, 'Benih Terong', 'Paket benih dalam bentuk polybag', 'benih', '50000', 999, 'Benih_Terong-min.png'),
+(24, 'Gunting Taman', 'Gunting untuk tanaman', 'alat', '75000', 34, 'Gunting_taman.jpg'),
+(25, 'Alat siram tanaman', 'Alat untuk menyiram', 'alat', '35000', 12, 'penyiram.jpg'),
+(26, 'Sarung tangan', 'Sarung tangan untuk bercocok tanam', 'alat', '65000', 17, 'Sarung_tangan.jpg'),
+(27, 'Sprayer', 'Alat untuk menyemprotkan disinfektan', 'alat', '50000', 11, 'Sprayer.jpg'),
+(28, 'Set cocok tanam', 'Set alat bercocok tanam', 'alat', '95000', 11, 'Sekop.JPG'),
+(29, 'Polybag', 'Polybag dengan 4 ukuran (1 paket berisi 100 polybag)', 'alat', '50000', 76, 'polybag.jpg'),
+(30, 'Pupuk cair dan fertilizer', '1,5 liter pupupk cair dan fertilizer', 'alat', '125000', 36, 'PH_Fertilizer_Pupuk_Organik_Cair.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_invoice`
+-- Struktur dari tabel `tb_invoice`
 --
 
 CREATE TABLE `tb_invoice` (
   `id_inv` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `alamat` text NOT NULL,
+  `no` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `via` varchar(50) NOT NULL,
   `tgl_pesan` datetime NOT NULL,
   `batas_byr` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_invoice`
+-- Dumping data untuk tabel `tb_invoice`
 --
 
-INSERT INTO `tb_invoice` (`id_inv`, `nama`, `alamat`, `tgl_pesan`, `batas_byr`) VALUES
-(1, 'M Ilyas Arman S', 'Sukoharjo ', '2021-06-23 06:41:44', '2021-06-24 06:41:44');
+INSERT INTO `tb_invoice` (`id_inv`, `nama`, `alamat`, `no`, `email`, `via`, `tgl_pesan`, `batas_byr`) VALUES
+(1, 'M Ilyas Arman S', 'Sukoharjo ', '0', '', '', '2021-06-23 06:41:44', '2021-06-24 06:41:44'),
+(2, 'Muhammad Ravi Firmansyah', 'Kriyan Rt:09/02 Kalinyamatan Jepara', '08986583928', 'ravi@gmail', 'bri', '2021-06-24 17:39:04', '2021-06-25 17:39:04'),
+(4, 'AdminDPC', 'Sidoarjo', '08986583928', 'wewe@gmail', 'bni', '2021-06-25 21:32:21', '2021-06-26 21:32:21');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_pengguna`
+-- Struktur dari tabel `tb_pengguna`
 --
 
 CREATE TABLE `tb_pengguna` (
@@ -118,17 +139,19 @@ CREATE TABLE `tb_pengguna` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_pengguna`
+-- Dumping data untuk tabel `tb_pengguna`
 --
 
 INSERT INTO `tb_pengguna` (`id_user`, `username`, `password`, `nama`, `alamat`, `level`, `foto`) VALUES
-(1, 'admin', 'admin123', 'AdminDPC', 'Semarang', 1, 'avatar04.png'),
-(3, 'mhmmdilyas', 'ilyas', 'M Ilyas Arman S', 'Sukoharjo', 2, '2.jpg');
+(1, 'admin', 'admin123', 'AdminDPC', 'Sidoarjo', 1, 'avatar04.png'),
+(2, 'ravi25', 'ravi123', 'ravi', 'jepara', 2, NULL),
+(3, 'mhmmdilyas', 'ilyas', 'M Ilyas Arman S', 'Sukoharjo', 2, '2.jpg'),
+(5, 'ravi11', 'ravi123', 'ravi', 'Jepara', 2, 'user2-160x160.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_pesan`
+-- Struktur dari tabel `tb_pesan`
 --
 
 CREATE TABLE `tb_pesan` (
@@ -140,10 +163,17 @@ CREATE TABLE `tb_pesan` (
   `pesan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `tb_pesan`
+--
+
+INSERT INTO `tb_pesan` (`id_pesan`, `id_user`, `nama`, `email`, `subjek`, `pesan`) VALUES
+(1, 1, 'Deni', 'dewa@gmail', 'iseng', 'hai');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_pesanan`
+-- Struktur dari tabel `tb_pesanan`
 --
 
 CREATE TABLE `tb_pesanan` (
@@ -156,18 +186,24 @@ CREATE TABLE `tb_pesanan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_pesanan`
+-- Dumping data untuk tabel `tb_pesanan`
 --
 
 INSERT INTO `tb_pesanan` (`id_pesanan`, `id_invoice`, `id_brg`, `nama_brg`, `jumlah`, `harga`) VALUES
 (1, 1, 2, 'Kentang ', 1, 100000),
 (2, 1, 3, 'Kol Putih', 1, 15000),
-(3, 1, 4, 'Tomat', 2, 100000);
+(3, 1, 4, 'Tomat', 2, 100000),
+(4, 2, 25, 'Alat siram tanaman', 1, 35000),
+(5, 2, 15, 'Benih Tomat', 1, 50000),
+(6, 3, 1, 'Wortel', 2, 15000),
+(7, 3, 2, 'Kentang ', 2, 100000),
+(8, 3, 12, 'Plastik Green House', 1, 15000),
+(9, 4, 2, 'Kentang ', 1, 100000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_supplier`
+-- Struktur dari tabel `tb_supplier`
 --
 
 CREATE TABLE `tb_supplier` (
@@ -179,7 +215,7 @@ CREATE TABLE `tb_supplier` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_supplier`
+-- Dumping data untuk tabel `tb_supplier`
 --
 
 INSERT INTO `tb_supplier` (`id_supplier`, `nama`, `alamat`, `telepon`, `deskripsi`) VALUES
@@ -192,100 +228,100 @@ INSERT INTO `tb_supplier` (`id_supplier`, `nama`, `alamat`, `telepon`, `deskrips
 --
 
 --
--- Indexes for table `tb_artikel`
+-- Indeks untuk tabel `tb_artikel`
 --
 ALTER TABLE `tb_artikel`
   ADD PRIMARY KEY (`id_artikel`);
 
 --
--- Indexes for table `tb_barang`
+-- Indeks untuk tabel `tb_barang`
 --
 ALTER TABLE `tb_barang`
   ADD PRIMARY KEY (`id_brg`);
 
 --
--- Indexes for table `tb_invoice`
+-- Indeks untuk tabel `tb_invoice`
 --
 ALTER TABLE `tb_invoice`
   ADD PRIMARY KEY (`id_inv`);
 
 --
--- Indexes for table `tb_pengguna`
+-- Indeks untuk tabel `tb_pengguna`
 --
 ALTER TABLE `tb_pengguna`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- Indexes for table `tb_pesan`
+-- Indeks untuk tabel `tb_pesan`
 --
 ALTER TABLE `tb_pesan`
   ADD PRIMARY KEY (`id_pesan`),
   ADD KEY `id_user` (`id_user`);
 
 --
--- Indexes for table `tb_pesanan`
+-- Indeks untuk tabel `tb_pesanan`
 --
 ALTER TABLE `tb_pesanan`
   ADD PRIMARY KEY (`id_pesanan`);
 
 --
--- Indexes for table `tb_supplier`
+-- Indeks untuk tabel `tb_supplier`
 --
 ALTER TABLE `tb_supplier`
   ADD PRIMARY KEY (`id_supplier`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tb_artikel`
+-- AUTO_INCREMENT untuk tabel `tb_artikel`
 --
 ALTER TABLE `tb_artikel`
-  MODIFY `id_artikel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_artikel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `tb_barang`
+-- AUTO_INCREMENT untuk tabel `tb_barang`
 --
 ALTER TABLE `tb_barang`
-  MODIFY `id_brg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_brg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
--- AUTO_INCREMENT for table `tb_invoice`
+-- AUTO_INCREMENT untuk tabel `tb_invoice`
 --
 ALTER TABLE `tb_invoice`
-  MODIFY `id_inv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_inv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `tb_pengguna`
+-- AUTO_INCREMENT untuk tabel `tb_pengguna`
 --
 ALTER TABLE `tb_pengguna`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `tb_pesan`
+-- AUTO_INCREMENT untuk tabel `tb_pesan`
 --
 ALTER TABLE `tb_pesan`
-  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `tb_pesanan`
+-- AUTO_INCREMENT untuk tabel `tb_pesanan`
 --
 ALTER TABLE `tb_pesanan`
-  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `tb_supplier`
+-- AUTO_INCREMENT untuk tabel `tb_supplier`
 --
 ALTER TABLE `tb_supplier`
   MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `tb_pesan`
+-- Ketidakleluasaan untuk tabel `tb_pesan`
 --
 ALTER TABLE `tb_pesan`
   ADD CONSTRAINT `tb_pesan_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tb_pengguna` (`id_user`);
